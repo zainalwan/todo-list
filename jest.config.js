5e6f29d4727cba55715c85197ae8dc5bb94ae49f
Zain Alwan Wima Irfani <zainalwan4@gmail.com>
2022-11-30 21:12:11 +0700

    Configure Jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: false,
+  collectCoverageFrom: [
+    './src/**/*.ts',
+    '!./src/migrations/*.ts',
+  ]
+};
