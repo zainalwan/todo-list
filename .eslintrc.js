67333255a5e3cd3d29054a675ba728246791a102
Zain Alwan Wima Irfani <zainalwan4@gmail.com>
2022-11-30 21:12:04 +0700

    Configure TypeScript & ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  "env": {
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "overrides": [
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      {
+        "ignoredNodes": [
+          "PropertyDefinition"
+        ]
+      }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "comma-dangle": [
+      "error",
+      "always-multiline"
+    ],
+    "comma-spacing": [
+      "error"
+    ],
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ],
+    "no-var": [
+      "error",
+    ],
+    "prefer-const": [
+      "off",
+    ],
+  }
+};
