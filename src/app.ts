eef278851311c8bfdea8ec6ed448900f1f1dfc9f
Zain Alwan Wima Irfani <zainalwan4@gmail.com>
2022-12-04 22:58:11 +0700

    Use morgan & helmet

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import { router as loginRouter } from './routes/login';
 import { router as logoutRouter } from './routes/logout';
 import { router as registerRouter } from './routes/register';
@@ -7,6 +9,8 @@ import { router as toDoRouter } from './routes/toDo';
 
 export const app = express();
 
+app.use(helmet());
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(cookieParser());
 
